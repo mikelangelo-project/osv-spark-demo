ed31e98794d4caa47698dbbe60e249511d436c69
Miha Pleško <miha.plesko@xlab.si>
2017-09-26 17:25:47 +0200

    Provide default bootcmd in meta/run.yaml
    With this commit we provide meta/run.yaml that sets the
    default boot command for the unikernel. The default boot
    is sllep for eternety since we're going to run the actual
    command via cloud-init.
    
    Signed-off-by: Miha Pleško <miha.plesko@xlab.si>

--- virtlet_deploy/compose-and-upload-unikernels.sh
+++ virtlet_deploy/compose-and-upload-unikernels.sh
@@ -4,7 +4,7 @@ IMAGESERVER_POD="${1:-imageserver-pod-id}"
 
 function fun::compose_and_upload {
   echo "Composing and uploading spark unikernel"
-  capstan package compose spark --update --size=1GB --run "--noshutdown /tools/cpiod.so --help" --pull-missing
+  capstan package compose spark --size=1GB --pull-missing
 
   echo "Copying to ---image-server---"
   kubectl cp "$HOME/.capstan/repository/spark/spark.qemu" "kube-system/$IMAGESERVER_POD:/usr/share/nginx/html/"
